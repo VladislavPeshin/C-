251aadd377b68ec4baa9fc6fc96aabde526c4733
VladislavPeshin <ruvladislav97@gmail.com>
2019-12-09 23:53:11 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,11 +10,23 @@ namespace taskBook
     {
         static void Main(string[] args)
         {
-            int a = 3;
-            int b = 5;
-            int c = 40;
-            int d = c-- - b * a;    // a=3  b=5  c=39  d=25
-            Console.WriteLine($"a={a}  b={b}  c={c}  d={d}");
+            //1.1 Вывести на экран число p с точностью до сотых.
+            Console.WriteLine(3.14);
+
+            //1.2 Вывести на экран число e (основание натурального логарифма) с точностью до десятых.
+            Console.WriteLine(2.7);
+
+            //1.3 Составить программу вывода на экран числа, вводимого с клавиатуры. Выводимому числу должно предшествовать сообщение "Вы ввели число".
+            Console.Write("");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вы ввели число: {number1}");
+
+            //1.4 Составить программу вывода на экран числа, вводимого с клавиатуры. После выводимого числа должно следовать сообщение " - вот какое число Выввели"
+            Console.Write("");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{number2} - вот какое число Вы ввели");
+
+
         }
     }
 }
